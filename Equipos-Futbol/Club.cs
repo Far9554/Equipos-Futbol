c5ccd28cbde4a26828c0e3c7540c6385f24f562b
marcbryan <marcboakyeflores98@gmail.com>
2023-10-13 17:16:35 +0200

    Clase Club

---
+++ Equipos-Futbol/Club.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equipos_Futbol
+{
+    internal class Club
+    {
+        private List<Equipo> equipos = new List<Equipo>();
+        private string nombre;
+
+        public List<Equipo> Equipos { get { return equipos; } set { equipos = value; } }
+
+        public string Nombre { get { return nombre; } set { nombre = value; } }
+
+        public Club() { }
+
+        public Club(string nombre)
+        {
+            this.nombre = nombre;
+        }
+    }
+}
